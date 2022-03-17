87916c481e035580704d649ffccb723754290da6
Austin Brumley <austinbrumley@austins-mbp.lan>
2022-03-17 13:36:51 -0600

    organization

--- austin-argocd/setup.sh
+++ austin-argocd/setup.sh
@@ -18,3 +18,11 @@ echo "Current argocd Admin password: is test-cna "
 echo 
 echo "Logging on to ArgoCD..."
 argocd login localhost:8080
+
+
+#install rollouts:
+echo "Applying Rollouts manifest to cluster:"
+kubectl apply -n argocd -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
+#create app
+echo "Creating app"
+argocd app create guestbook-ui-bg --repo https://github.com/abrumley-mw/argocd.git --path austin-argocd/apps/blue-green --dest-server https://kubernetes.default.svc --dest-namespace argocd
